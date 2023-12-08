6068b5411885e3ef1bd538fae98d8e98d5981902
NEo0o0 <nathdanai.nepal@gmail.com>
2023-12-08 11:46:32 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,25 @@
 using namespace std;
 
 int main(){
+    int i=0,E=0,O=0,r=1;
+    int num[100000];
+    while (r!=0) {
     cout << "Enter an integer: ";
+    cin>>num[i];
+    if(num[i]==0){
+        r=0;
+    }
+    if(num[i]%2==0){
+        E++;
+    }
+    else{
+        O++;
+    }
+    i++;
+    }
     cout << "#Even numbers = ";
+    cout<< E;
     cout << "#Odd numbers = ";
+    cout<<O;
     return 0;
 }
